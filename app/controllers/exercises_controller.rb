01e51606af0972bceb7565dd0ecaacaf7ccbed16
dai <sugerbig15@gmail.com>
2024-02-15 02:32:28 +0900

    4回目の課題提出です

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -17,15 +17,17 @@ class ExercisesController < ApplicationController
     # 【要件】配達先の一番多い住所を返すこと
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
-    @address = Address.joins(:orders).select(MAX(:orders))
-
+    @address = Address.joins(:orders)
+      .select('addresses.*, COUNT(orders.*) AS number_of_orders')
+      .group('addresses.id').order('orders_count DESC').first
   end
 
   def exercise4 
     # 【要件】一番お金を使っている顧客を返すこと
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
-    @customer = Customer
-
+    @customer = Customer.joins(orders: :foods)
+      .select('customers.*, SUM(foods.price)')
+      .order('foods_price_sum DESC').first
   end
 end
